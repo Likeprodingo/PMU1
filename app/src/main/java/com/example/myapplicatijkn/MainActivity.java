ced04bd15c75238ad9b9f2a84f2b0519c162bb1e
Likeprodingo <alshiba1@mail.ru>
2021-04-30 14:02:38 +0300

    Strings

---
+++ app/src/main/java/com/example/myapplicatijkn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplicatijkn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+}
